93b821a13c2b0b9457fbf3e6f33dea5580612db4
andrewnofear <nefedov1000@yandex.ru>
2024-07-31 17:32:39 +0300

    secondCommit

--- precode.sh
+++ precode.sh
@@ -18,7 +18,7 @@ touch dir2/empty
 touch dir2/hello.sh
 set +H
 echo '#!/bin/bash
-echo "$1 $2, привет!"' > dir2/hello.sh #без  $2 не выведет второе слово "студентам"
+echo "$1, привет!"' > dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 dir2/hello.sh
@@ -32,10 +32,10 @@ find -name "*.txt" > dir1/summary.txt
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
 cat dir2/list.txt >> dir1/summary.txt
 # определяем переменную окружения NAME со значением "Всем студентам"
-NAME='Всем студентам'
+export NAME='Всем студентам'
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-sh dir2/hello.sh $NAME >> dir1/summary.txt
+sh dir2/hello.sh "$NAME" >> dir1/summary.txt
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 mv dir1/summary.txt "Практическое задание"
 # выводим на консоль содержимое файла task/Практическое задание
@@ -43,7 +43,7 @@ cat "Практическое задание"
 
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
-grep "dir" "Практическое задание" || sort
+grep "dir" "Практическое задание" | sort
 # меняем текущую директорию на родительскую для task
 cd ..
 # удаляем директорию task со всем содержимым
